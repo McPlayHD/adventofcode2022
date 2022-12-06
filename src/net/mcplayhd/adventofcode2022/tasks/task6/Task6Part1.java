feb695b5ef2e0292ebfd3dd8bae2dcbc35745f92
McPlayHD <McPlayHD@gmx.ch>
2022-12-06 08:43:28 +0100

    solved day 6

---
+++ src/net/mcplayhd/adventofcode2022/tasks/task6/Task6Part1.java
@@ -0,0 +1,29 @@
+package net.mcplayhd.adventofcode2022.tasks.task6;
+
+import net.mcplayhd.adventofcode2022.tasks.Task;
+
+public class Task6Part1 extends Task {
+    @Override
+    public String getResult(String[] input) {
+        String line = input[0];
+        char[] cArray = line.toCharArray();
+        int amount = 4;
+        for (int i = amount - 1; i < cArray.length; i ++) {
+            if (!anyTheSame(cArray, i - (amount - 1), i)) {
+                return Integer.toString(i + 1);
+            }
+        }
+        return null;
+    }
+
+    boolean anyTheSame(char[] cArray, int begin, int end) {
+        for (int i = 0; i <= end - begin; i ++) {
+            for (int j = i + 1; j <= end - begin; j ++) {
+                if (cArray[i + begin] == cArray[j + begin]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
